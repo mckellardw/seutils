6d988865a984a43bc4cbe87f8bd57515adde4801
mckellardw <dwm269@cornell.edu>
2023-03-04 18:39:38 -0500

    `seuplots` and README updates

--- R/seuplots.R
+++ R/seuplots.R
@@ -1,245 +1,7 @@
-###############################
+#############################################################################
 # seuplots -  Seurat-compatible plotting functions
 # Written by: David McKellar
-# version: 1.0
-
 #############################################################################
-## Utils --------------------------------------------------------------------
-
-
-# Split violin plots
-.GeomSplitViolin <- ggproto(
-  "GeomSplitViolin",
-  GeomViolin,
-  draw_group = function(self, data, ..., draw_quantiles = NULL) {
-
-    data <-
-      transform(
-        data,
-        xminv = x - violinwidth * (x - xmin),
-        xmaxv = x + violinwidth * (xmax - x)
-      )
-
-    grp <- data[1, "group"]
-
-    newdata <-
-      plyr::arrange(transform(data, x = if (grp %% 2 == 1)
-        xminv
-        else
-          xmaxv), if (grp %% 2 == 1)
-            y
-        else-y)
-
-    newdata <- rbind(newdata[1,], newdata, newdata[nrow(newdata),], newdata[1,])
-
-    newdata[c(1, nrow(newdata) - 1, nrow(newdata)), "x"] <- round(newdata[1, "x"])
-
-    if (length(draw_quantiles) > 0 &
-        !scales::zero_range(range(data$y))) {
-      stopifnot(all(draw_quantiles >= 0), all(draw_quantiles <=
-                                                1))
-      quantiles <-
-        ggplot2:::create_quantile_segment_frame(data, draw_quantiles)
-      aesthetics <-
-        data[rep(1, nrow(quantiles)), setdiff(names(data), c("x", "y")), drop = FALSE]
-      aesthetics$alpha <-
-        rep(1, nrow(quantiles))
-      both <- cbind(quantiles, aesthetics)
-      quantile_grob <-
-        GeomPath$draw_panel(both, ...)
-      ggplot2:::ggname("geom_split_violin",
-                       grid::grobTree(GeomPolygon$draw_panel(newdata, ...), quantile_grob))
-    }
-    else {
-      ggplot2:::ggname("geom_split_violin",
-                       GeomPolygon$draw_panel(newdata, ...))
-    }
-  }
-)
-
-.geom_split_violin <-
-  function(mapping = NULL,
-           data = NULL,
-           stat = "ydensity",
-           position = "identity",
-           ...,
-           draw_quantiles = NULL,
-           trim = TRUE,
-           scale = "area",
-           na.rm = FALSE,
-           show.legend = NA,
-           inherit.aes = TRUE) {
-    layer(
-      data = data,
-      mapping = mapping,
-      stat = stat,
-      geom = .GeomSplitViolin,
-      position = position,
-      show.legend = show.legend,
-      inherit.aes = inherit.aes,
-      params = list(
-        trim = trim,
-        scale = scale,
-        draw_quantiles = draw_quantiles,
-        na.rm = na.rm,
-        ...
-      )
-    )
-  }
-
-#############################################################################
-## Differential Gene Expression Plots ---------------------------------------
-
-# Build volcano plot after running FindMarkers()
-#   *Note- requires Seurat v4 or greater (b/c colnames in the output file changed for some reason...)
-ggVolcano_v2 <- function(
-  markers=NULL,
-  expression=NULL,
-  seu=NULL,
-  logFC_filter = 1,
-  neg.log.pval.Thresh=50,
-  pct.thresh = 0.4,
-  plotTitle='Volcano Plot',
-  pseudocount=10^-300,
-  fill.cols = c("#000000","#cf4c59","#2c30a8"),
-  xlim=NULL, ylim=NULL,
-  genes=NULL,
-  gene.text.size=6, 
-  repel=T, 
-  nudge_x=-0.1,
-  dot.scale=1,
-  line.width=0.5, 
-  segment.color="gray",
-  pt.size=1,
-  pt.alpha=1
-){
-  # TODO: add split.by - generalize?
-
-  require(ggrepel)
-
-  if(is.null(rownames(markers))){
-    stop("Differential gene expression matrix needs gene names as rownames()...")
-  }
-  if(plotTitle=='Volcano Plot'){
-    cat('Warning: no plot title given...\n')
-  }
-
-  # Build data.frame ####
-  df <- data.frame(
-    p_val_adj = -log10(markers$p_val_adj+pseudocount),
-    avg_log2FC = markers$avg_log2FC,
-    genes = rownames(markers),
-    p_val_filter = -log10(markers$p_val_adj+pseudocount)>neg.log.pval.Thresh, # Values that pass p value threshold
-    FC_filter.low = markers$avg_log2FC < -1*logFC_filter,
-    FC_filter.high= markers$avg_log2FC > logFC_filter
-  )
-
-  if(is.null(seu)){
-    df$pct =
-      apply(markers,1,
-            function(X){
-              if(as.numeric(X["avg_log2FC"])>0){
-                (return(as.numeric(X["pct.1"])))
-              }else{
-                return(as.numeric(X["pct.2"]))
-              }
-            }
-      )
-
-    df$expr.filter = df$pct > pct.thresh # which genes to label
-  }else{
-    #TODO: add in gene expression data
-  }
-
-  # Add colors ####
-  df$cols <- rep(x='none', times=length(df$genes))
-  df$cols[df$p_val_filter & df$FC_filter.high] <- 'high'
-  df$cols[df$p_val_filter & df$FC_filter.low] <- 'low'
-  df$cols <- factor(df$cols,levels=c("none", "high", "low"))
-
-  message(table(df$colors!="none" & df$expr.filter)[2], "genes drawn \n")
-  # Build ggplot object ####
-  out.gg <- ggplot(
-    data = df,
-    aes(x=avg_log2FC, y=p_val_adj)
-  ) +
-    geom_hline(# pval dotted line
-      yintercept = neg.log.pval.Thresh,
-      linetype='dashed',
-      color='lightgray', 
-      size = line.width
-    )  +
-    geom_vline(# logFC dotted line
-      xintercept = c(logFC_filter, -logFC_filter),
-      linetype='dashed',
-      color='lightgray', 
-      size = line.width
-    ) +
-    geom_vline(
-      xintercept = 0, 
-      color='black',
-      size = line.width
-    ) +
-    geom_point(
-      pch=21, size=pt.size, alpha=pt.alpha,
-      aes(
-        fill = cols, col=cols
-        # size = pct # scale dot size according to relative pct expression
-      )
-    )
-  if(repel){
-    out.gg <- out.gg +
-      geom_text_repel(
-        data=df[df$cols!="none" & df$expr.filter,],
-        size=gene.text.size*(5/14), #convert to same scale as normal ggplot
-        segment.size=0.25,
-        segment.alpha = 0.8,
-        segment.color = segment.color,
-        point.padding = 0.4,
-        aes(
-          label=genes,
-          col=cols
-        )
-      )
-  }else{
-    out.gg <- out.gg + 
-      geom_text(
-        data=df[df$cols=="both" & df$expr.filter,],
-        size=gene.text.size*(5/14), #convert to same scale as normal ggplot
-        # position=position_dodge(width = 1),
-        nudge_x = nudge_x,
-        aes(
-          label=genes,
-          col=cols
-        )
-    )
-  }
-  # Finish plot ####
-  out.gg <- out.gg +
-    labs(
-      x="log2_FC",
-      y="-log10(adj._p_val)",
-      size = "Pct. Expr."
-    ) +
-    ggtitle(plotTitle) +
-    guides(color=FALSE, fill=FALSE) +
-    theme_minimal() +
-    theme(
-      panel.grid.major = element_blank(),
-      panel.grid.minor = element_blank(),
-      panel.background = element_blank(),
-      axis.line = element_blank(),#element_line(colour = "black", size = line.width),
-      axis.ticks=element_line(colour = "black", size = line.width),
-      panel.border = element_rect(color='black', size=line.width, fill=NA),
-      plot.title = element_text(face="bold", hjust=0.5)
-    ) +
-    scale_fill_manual(values=fill.cols,aesthetics = c("col","fill"))+
-    scale_x_continuous(expand=c(0.01,0.01))+
-    scale_y_continuous(expand=c(0.01,0.01))
-
-  return(out.gg)
-}
-
 
 #############################################################################
 ## QC plots  ----------------------------------------------------------------
@@ -302,7 +64,7 @@ kneePlot <- function(
 
 
 # Generate feature plots given a list of Visium/Seurat objects
-visListPlot <- function(
+seuListPlot <- function(
   seu.list,
   features=NULL,
   alt.titles=NULL, # alternative titles for genes/features being passed
@@ -716,8 +478,8 @@ visListPlot <- function(
 
 
 # Plot co-expression of any two genes/features
-## Built on top of visListPlot()
-visCoMap <- function(
+## Built on top of seuListPlot()
+seuCoMap <- function(
   seu.list,
   features=NULL, # pair of genes 
   alt.titles=NULL, # alternative titles for genes/features being passed
@@ -835,7 +597,7 @@ visCoMap <- function(
       return(SEU)
     }
   ) %>%
-    visListPlot( # Plot all three with visListPlot
+    seuListPlot( # Plot all three with seuListPlot
       # seu.list=seu.list,
       features=c(features,coex.name), # pass two features plus the co-expression values
       alt.titles=c(alt.titles, coex.name),
@@ -1055,7 +817,6 @@ visCoMap <- function(
   }
 }
 
-
 # Generate split violin plots for Visium data to look at co-occurence of cell types & gene expression
 #   Co-occurence is based on BayesPrism theta values
 visCoocVlnPlot<-function(
@@ -1150,4 +911,157 @@ visCoocVlnPlot<-function(
     )
 
     return(out.plot)
+}
+
+#############################################################################
+## Differential Gene Expression Plots ---------------------------------------
+
+# Build volcano plot after running FindMarkers()
+#   *Note- requires Seurat v4 or greater (b/c colnames in the output file changed for some reason...)
+ggVolcano <- function(
+  markers=NULL,
+  expression=NULL,
+  seu=NULL,
+  logFC_filter = 1,
+  neg.log.pval.Thresh=50,
+  pct.thresh = 0.4,
+  plotTitle='Volcano Plot',
+  pseudocount=10^-300,
+  fill.cols = c("#000000","#cf4c59","#2c30a8"),
+  xlim=NULL, ylim=NULL,
+  genes=NULL,
+  gene.text.size=6, 
+  repel=T, 
+  nudge_x=-0.1,
+  dot.scale=1,
+  line.width=0.5, 
+  segment.color="gray",
+  pt.size=1,
+  pt.alpha=1
+){
+  # TODO: add split.by - generalize?
+
+  require(ggrepel)
+
+  if(is.null(rownames(markers))){
+    stop("Differential gene expression matrix needs gene names as rownames()...")
+  }
+  if(plotTitle=='Volcano Plot'){
+    cat('Warning: no plot title given...\n')
+  }
+
+  # Build data.frame ####
+  df <- data.frame(
+    p_val_adj = -log10(markers$p_val_adj+pseudocount),
+    avg_log2FC = markers$avg_log2FC,
+    genes = rownames(markers),
+    p_val_filter = -log10(markers$p_val_adj+pseudocount)>neg.log.pval.Thresh, # Values that pass p value threshold
+    FC_filter.low = markers$avg_log2FC < -1*logFC_filter,
+    FC_filter.high= markers$avg_log2FC > logFC_filter
+  )
+
+  if(is.null(seu)){
+    df$pct =
+      apply(markers,1,
+            function(X){
+              if(as.numeric(X["avg_log2FC"])>0){
+                (return(as.numeric(X["pct.1"])))
+              }else{
+                return(as.numeric(X["pct.2"]))
+              }
+            }
+      )
+
+    df$expr.filter = df$pct > pct.thresh # which genes to label
+  }else{
+    #TODO: add in gene expression data
+  }
+
+  # Add colors ####
+  df$cols <- rep(x='none', times=length(df$genes))
+  df$cols[df$p_val_filter & df$FC_filter.high] <- 'high'
+  df$cols[df$p_val_filter & df$FC_filter.low] <- 'low'
+  df$cols <- factor(df$cols,levels=c("none", "high", "low"))
+
+  message(table(df$colors!="none" & df$expr.filter)[2], "genes drawn \n")
+  # Build ggplot object ####
+  out.gg <- ggplot(
+    data = df,
+    aes(x=avg_log2FC, y=p_val_adj)
+  ) +
+    geom_hline(# pval dotted line
+      yintercept = neg.log.pval.Thresh,
+      linetype='dashed',
+      color='lightgray', 
+      size = line.width
+    )  +
+    geom_vline(# logFC dotted line
+      xintercept = c(logFC_filter, -logFC_filter),
+      linetype='dashed',
+      color='lightgray', 
+      size = line.width
+    ) +
+    geom_vline(
+      xintercept = 0, 
+      color='black',
+      size = line.width
+    ) +
+    geom_point(
+      pch=21, size=pt.size, alpha=pt.alpha,
+      aes(
+        fill = cols, col=cols
+        # size = pct # scale dot size according to relative pct expression
+      )
+    )
+  if(repel){
+    out.gg <- out.gg +
+      geom_text_repel(
+        data=df[df$cols!="none" & df$expr.filter,],
+        size=gene.text.size*(5/14), #convert to same scale as normal ggplot
+        segment.size=0.25,
+        segment.alpha = 0.8,
+        segment.color = segment.color,
+        point.padding = 0.4,
+        aes(
+          label=genes,
+          col=cols
+        )
+      )
+  }else{
+    out.gg <- out.gg + 
+      geom_text(
+        data=df[df$cols=="both" & df$expr.filter,],
+        size=gene.text.size*(5/14), #convert to same scale as normal ggplot
+        # position=position_dodge(width = 1),
+        nudge_x = nudge_x,
+        aes(
+          label=genes,
+          col=cols
+        )
+    )
+  }
+  # Finish plot ####
+  out.gg <- out.gg +
+    labs(
+      x="log2_FC",
+      y="-log10(adj._p_val)",
+      size = "Pct. Expr."
+    ) +
+    ggtitle(plotTitle) +
+    guides(color=FALSE, fill=FALSE) +
+    theme_minimal() +
+    theme(
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      panel.background = element_blank(),
+      axis.line = element_blank(),#element_line(colour = "black", size = line.width),
+      axis.ticks=element_line(colour = "black", size = line.width),
+      panel.border = element_rect(color='black', size=line.width, fill=NA),
+      plot.title = element_text(face="bold", hjust=0.5)
+    ) +
+    scale_fill_manual(values=fill.cols,aesthetics = c("col","fill"))+
+    scale_x_continuous(expand=c(0.01,0.01))+
+    scale_y_continuous(expand=c(0.01,0.01))
+
+  return(out.gg)
 }
--- README.md
+++ README.md
@@ -9,55 +9,58 @@ Utility functions for Seurat
 
 ### Generic Seurat utils
 
-#### `Features()`
+### `Features()`
 Quickly grab feature names from any `Assay` within a `Seurat` object - analogous to `Seurat::Cells()`
 
-#### `grepGene()`
+### `grepGene()`
 Quick function to look for genes in a `Seurat` object that match a certain pattern. Can also set a `filter.pattern` to exclude genes which contain the string. Built on `grep` and `Seurat`
 
-#### `ens2gene()`
+### `ens2gene()`
 Convert a list of ensembl IDs to gene symbols. Compatible with outputs from `biomaRt`.
 
-#### `npcs()`
+### `npcs()`
 Calculate how many principal components account for X% of the variance.
 
-#### `collapseMultimappers()`
+### `collapseMultimappers()`
 Quickly collapse multimapper genes in a `Seurat` assay (non-unique features, marked by a "." in the feature name). Note, the literal regex used is `\\.`
 
-#### `seuPreProcess()`
+### `seuPreProcess()`
 Generic single-cell pipeline, based on [this vignette](https://satijalab.org/seurat/articles/pbmc3k_tutorial.html) from the Satija Lab
 
-#### `AddCellTypeIdents()`
+### `AddCellTypeIdents()`
 Add cell type labels given a clustering output
 
 
-### Spatial tools
-#### `addSpatialLocation()`
+## Spatial tools
+### `addSpatialLocation()`
 Add spatial location for each bead/spot within a Seurat object. Written to work with the whitelists from 10x Genomics (for Visium) and Curio Biosciences (for Seeker/SlideSeq)
 
-#### `removeSpatialSinglets()`
+### `removeSpatialSinglets()`
 Strategy to remove singlets based on spatial position/nearest neighbors - filters out beads/spots with fewer than `K` neighbors with `D` distance units
 
-#### `rotateClockwise90N()`
+### `rotateClockwise90N()`
 Rotate the spatial embedding of a sample clockwise, 90 degrees, `N` times (i.e. N=2 to rotate 180 degrees clockwise). Useful if you have Visium/SlideSeq samples that were sectioned in different orientations.
 
-#### `spatialRel2Abs()`
+### `spatialRel2Abs()`
 Convert relative (row/col) positions to absolute (X/Y) positions
 
-#### `subsetLoupeJson()`
+### `subsetLoupeJson()`
 Subset Visium Seurat object based on spots manually selected via loupe browser
 
 
-### Single-cell
+## Single-cell
 TODO
 
 ## **seuplots.R** (seurat plots)
-Additional plots not included in Seurat including
+Additional plots not included in Seurat
 
-#### `visListPlot()`
-Wrapper function for neatly plotting multiple Visium objects and multiple features into a grid. Input is a `list()` of `Seurat` objects. Includes automatic color scaling and lots of options for customization. Built on `patchwork`. Combine with `coord_fixed()` to maintain true spatial coordinates.
+### `seuListPlot()`
+Wrapper function for neatly plotting multiple Visium objects and multiple features into a grid. Input is a `list()` of `Seurat` objects. Includes automatic color scaling across objects and lots of options for customization. Built on `patchwork`. Combine with `coord_fixed()` to maintain true spatial coordinates. See [vignettes](https://github.com/mckellardw/seutils/blob/main/vignettes/plotting_with_seuListPlot.Rmd) for details.
+
+#TODO- update args
+#TODO- add compatibility with standard spatial handling by Seurat authors
 ```
-visListPlot <- function(
+seuListPlot <- function(
   seu.list, # list of Seurat objects
   features=NULL, # genes/metadata entries/etc to plot
   alt.titles=NULL, # alternative titles for genes/features being passed
@@ -80,17 +83,17 @@ visListPlot <- function(
   verbose=FALSE
 )
 ```
-**Usage notes on `visListPlot()`:**
+**Usage notes on `seuListPlot()`:**
 - Looks for the spatial position in the `reduction` passed (Check `SEU@reductions`), which means this also works with PCA/UMAP/PHATE/etc embeddings!
 - Only works with **continuous** variables for now (i.e. gene expression, nCounts, deconvolution estimates) - does NOT work with cluster IDs
 - Keeps the same color scale for all samples in each feature; use `colormap.same.scale=T` to force the same color scale across all samples AND features (i.e, when working with scaled gene expression values)
 - The `na.value` is the color used for cells/spots/beads where the feature value is either missing or `NA`. I recommend using the default light gray option (`gray(0.85)`)
 
 
-#### `visCoMap()`
-Plot co-expression of any two genes/features, where "co-expression" can be abstracted to an generic function (default is just `prod`, to see expression of Gene_A times expression of Gene_B). Built on top of visListPlot(), so it also takes a list of `Seurat` objects.
+#### `seuCoMap()`
+Plot co-expression of any two genes/features, where "co-expression" can be abstracted to an generic function (default is just `prod`, to see expression of Gene_A times expression of Gene_B). Built on top of seuListPlot(), so it also takes a list of `Seurat` objects.
 ```
-visCoMap <- function(
+seuCoMap <- function(
   seu.list,
   features=NULL, # pair of genes
   alt.titles=NULL, # alternative titles for genes/features being passed
